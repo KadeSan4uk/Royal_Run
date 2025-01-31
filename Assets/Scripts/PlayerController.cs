b667875e7f37fb4b5317bcbc8182878404e9f3ca
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-01-31 15:27:40 +0200

    Restrain Player Using Clamp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float xClamp = 3f;
+    [SerializeField] float zMaxClamp = 3f;
+    [SerializeField] float zMinClamp = 3f;
 
     Vector2 movement;
     Rigidbody rigiBody;
@@ -24,6 +27,9 @@ public class PlayerController : MonoBehaviour
         Vector3 currentPosition = rigiBody.position;// получаем текущую позицию
         Vector3 moveDirrection = new Vector3(movement.x, 0f, movement.y);// создаем направление движения
         Vector3 newPosition = currentPosition + moveDirrection * (moveSpeed * Time.fixedDeltaTime);// вычесляем новую позицию
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -xClamp, xClamp);
+        newPosition.z = Mathf.Clamp(newPosition.z, -zMinClamp, zMaxClamp);
 
         rigiBody.MovePosition(newPosition);// двигаем Rigibody
     }
