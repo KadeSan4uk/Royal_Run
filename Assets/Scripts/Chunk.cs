370160fb171c1f3323666b17bac295d3a928bb6b
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-02-04 15:56:24 +0200

    Lane Pickups

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,20 +1,64 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Chunk : MonoBehaviour
 {
     [SerializeField] GameObject fencePrefab;
+    [SerializeField] GameObject applePrefab;
+    [SerializeField] GameObject coinPrefab;
+    [SerializeField] float appleSpawnChance = 0.3f;
+    [SerializeField] float coinSpawnChance = 0.5f;
+    [SerializeField] float coinSeperationLenght = 2f;
+
+
+
     [SerializeField] float[] lanes = { -2.5f, 0f, 2.5f };
+
+    List<int> availibleLanes = new List<int> { 0, 1, 2 };
 
     private void Start()
     {
         SpawbFence();
+        SpawnApple();
+        SpawnCoins();
+    }
+
+    void SpawnCoins()
+    {
+        if (Random.value > coinSpawnChance || availibleLanes.Count <= 0) return;
+
+        int selectedLane = SelectLane();
+
+        int maxCoinsToSpawn = 6;
+        int coinsToSpawn = Random.Range(1, maxCoinsToSpawn);
+
+        float topOfChunkZPos = transform.position.z + (coinSeperationLenght * 2f);
+
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+            float spawnPositionZ = topOfChunkZPos - (i * coinSeperationLenght);
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, spawnPositionZ);
+            Instantiate(coinPrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
+
+
+    }
+
+    void SpawnApple()
+    {
+        if (Random.value > appleSpawnChance || availibleLanes.Count <= 0) return;
+
+        int selectedLane = SelectLane();
+
+        Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+        Instantiate(applePrefab, spawnPosition, Quaternion.identity, this.transform);
     }
 
     void SpawbFence()
     {
-        List<int> availibleLanes = new List<int> { 0, 1, 2 };
+
 
         int fencesToSpawn = Random.Range(0, lanes.Length);
 
@@ -22,14 +66,20 @@ public class Chunk : MonoBehaviour
         {
             if (availibleLanes.Count <= 0) break;
 
-            int randomLaneIndex = Random.Range(0, availibleLanes.Count);
-            int selectedLane = availibleLanes[randomLaneIndex];
-            availibleLanes.RemoveAt(randomLaneIndex);
+            int selectedLane = SelectLane();
 
             Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
             Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
         }
 
 
+    }
+
+    int SelectLane()
+    {
+        int randomLaneIndex = Random.Range(0, availibleLanes.Count);
+        int selectedLane = availibleLanes[randomLaneIndex];
+        availibleLanes.RemoveAt(randomLaneIndex);
+        return selectedLane;
     }
 }
