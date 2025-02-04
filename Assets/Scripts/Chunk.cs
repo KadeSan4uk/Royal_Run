09cc511d420a352b021e6e4d1d811deb3d7dfbd4
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-02-04 15:12:52 +0200

    Line Detection

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Chunk : MonoBehaviour
 {
@@ -12,8 +14,22 @@ public class Chunk : MonoBehaviour
 
     void SpawbFence()
     {
-        int randomLaneIndex = Random.Range(0, lanes.Length);
-        Vector3 spawnPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z);
-        Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        List<int> availibleLanes = new List<int> { 0, 1, 2 };
+
+        int fencesToSpawn = Random.Range(0, lanes.Length);
+
+        for (int i = 0; i < fencesToSpawn; i++)
+        {
+            if (availibleLanes.Count <= 0) break;
+
+            int randomLaneIndex = Random.Range(0, availibleLanes.Count);
+            int selectedLane = availibleLanes[randomLaneIndex];
+            availibleLanes.RemoveAt(randomLaneIndex);
+
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
+
+
     }
 }
