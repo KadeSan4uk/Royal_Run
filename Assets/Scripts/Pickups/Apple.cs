0587ce3166b0e7e03e2e3a3ffcfd54e7fe2286af
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-02-05 21:07:10 +0200

    Modifying Physics

--- Assets/Scripts/Pickups/Apple.cs
+++ Assets/Scripts/Pickups/Apple.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class Apple : Pickup
 {
+    [SerializeField] float adjustChengeMoveSpeedAmount = 3f;
+
+    LevelGenerator levelGenerator;
+
+    private void Start()
+    {
+        levelGenerator = FindFirstObjectByType<LevelGenerator>();
+    }
+
     protected override void OnPickup()
     {
-        Debug.Log("Power up!");
+        levelGenerator.ChangeChunkMoveSpeed(adjustChengeMoveSpeedAmount);
     }
 }
--- Assets/Scripts/Player/PlayerCollisionHanrdler.cs
+++ Assets/Scripts/Player/PlayerCollisionHanrdler.cs
@@ -6,20 +6,30 @@ public class PlayerCollisionHanrdler : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] float collisionCooldown = 1f;
+    [SerializeField] float adjustChengeMoveSpeedAmount = -2f;
 
+    const string hitString = "Hit";
     float cooldownTimer = 0;
+
+    LevelGenerator levelGenerator;
+
+    private void Start()
+    {
+        levelGenerator = FindFirstObjectByType<LevelGenerator>();
+    }
 
     private void Update()
     {
         cooldownTimer += Time.deltaTime;
     }
 
-    const string hitString = "Hit";
+
 
     void OnCollisionEnter(Collision other)
     {
         if (cooldownTimer < collisionCooldown) return;
 
+        levelGenerator.ChangeChunkMoveSpeed(adjustChengeMoveSpeedAmount);
         animator.SetTrigger(hitString);
         cooldownTimer = 0f;
     }
