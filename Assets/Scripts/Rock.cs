566ee6dbc1942fe29fb0a752ccecd95c0eb5866a
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-02-12 15:20:18 +0200

    Cinemachine Screen Shake

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,22 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] float shakeModifer = 10f;
+
+    CinemachineImpulseSource cinemachineImpulseSource;
+
+    private void Awake()
+    {
+        cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        float shakeIntensity = (1f / distance) * shakeModifer;
+        shakeIntensity = Mathf.Min(shakeIntensity, 1f);
+        cinemachineImpulseSource.GenerateImpulse(shakeIntensity);
+    }
+}
