47f0bb06b84b5740d2a338c6a3b6349db4a2200e
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-02-12 15:46:34 +0200

    Rock Collision FX pause

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -3,20 +3,36 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+    [SerializeField] ParticleSystem collisionParticleSystem;
+    [SerializeField] AudioSource boulderSmashAudioSource;
     [SerializeField] float shakeModifer = 10f;
 
     CinemachineImpulseSource cinemachineImpulseSource;
 
-    private void Awake()
+    void Awake()
     {
         cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision other)
+    {
+        FireImpulse();
+        CollisionFX(other);
+    }
+
+    void FireImpulse()
     {
         float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
         float shakeIntensity = (1f / distance) * shakeModifer;
         shakeIntensity = Mathf.Min(shakeIntensity, 1f);
         cinemachineImpulseSource.GenerateImpulse(shakeIntensity);
+    }
+
+    void CollisionFX(Collision other)
+    {
+        ContactPoint contactPoint = other.contacts[0];
+        collisionParticleSystem.transform.position = contactPoint.point;
+        collisionParticleSystem.Play();
+        boulderSmashAudioSource.Play();
     }
 }
